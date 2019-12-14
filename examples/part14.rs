84624863381eb9f20a4da112738cb57b399d272e
sim82 <simberger@gmail.com>
2019-12-14 20:34:41 +0100

    day 14

--- examples/part14.rs
+++ examples/part14.rs
@@ -6,11 +6,11 @@ use std::ops::Mul;
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct Term {
     element: String,
-    num: i32,
+    num: i64,
 }
 
 impl Term {
-    pub fn new(element: &str, num: i32) -> Self {
+    pub fn new(element: &str, num: i64) -> Self {
         Term {
             element: element.into(),
             num,
@@ -19,7 +19,7 @@ impl Term {
     pub fn from_str(s: &str) -> Self {
         let mut s = s.trim().split(" ");
         Term {
-            num: s.next().unwrap().parse::<i32>().unwrap(),
+            num: s.next().unwrap().parse::<i64>().unwrap(),
             element: s.next().unwrap().into(),
         }
     }
@@ -31,16 +31,16 @@ impl Display for Term {
     }
 }
 
-impl Mul<i32> for Term {
+impl Mul<i64> for Term {
     type Output = Term;
-    fn mul(self, other: i32) -> Term {
+    fn mul(self, other: i64) -> Term {
         Term::new(&self.element, self.num * other)
     }
 }
 
-impl Mul<i32> for &Term {
+impl Mul<i64> for &Term {
     type Output = Term;
-    fn mul(self, other: i32) -> Term {
+    fn mul(self, other: i64) -> Term {
         Term::new(&self.element, self.num * other)
     }
 }
@@ -51,7 +51,7 @@ trait Reactions {
 
 impl Reactions for HashMap<Term, Vec<Term>> {
     fn requirements(&self, output: &Term) -> (Vec<Term>, Term) {
-        println!("want {}", output);
+        // println!("want {}", output);
         for kv in self.iter() {
             if kv.0.element == output.element {
                 let num_reactions =
@@ -69,7 +69,7 @@ fn only_ore(terms: &Vec<Term>) -> bool {
     terms.iter().filter(|x| x.element != "ORE").count() == 0
 }
 fn squash_terms(terms: &Vec<Term>) -> Vec<Term> {
-    let mut m = HashMap::<String, i32>::new();
+    let mut m = HashMap::<String, i64>::new();
     for term in terms.iter() {
         match m.get_mut(&term.element) {
             Some(mt) => {
@@ -84,23 +84,14 @@ fn squash_terms(terms: &Vec<Term>) -> Vec<Term> {
     }
     m.iter().map(|kv| Term::new(kv.0, *kv.1)).collect()
 }
-fn main() {
-    let mut reactions = HashMap::new();
-    for line in stdin().lock().lines() {
-        let line = line.unwrap();
-        let mut split = line.split("=>"); //.collect();
-        let input = split.next().unwrap();
-        let output = Term::from_str(split.next().unwrap());
-        let input_elements: Vec<_> = input.split(",").map(|x| Term::from_str(&x[..])).collect();
 
-        reactions.insert(output, input_elements);
-    }
-    println!("{:?}", reactions);
-
+fn eval(target_term: &Term, reactions: &HashMap<Term, Vec<Term>>) -> i64 {
+    println!("===============================\n{}", target_term);
     let mut queue = VecDeque::new();
     queue.push_back((Term::new("FUEL", 1), "".to_string()));
-    let mut input: Vec<Term> = reactions.requirements(&Term::new("FUEL", 1)).0;
-    let mut output: Vec<Term> = vec![Term::new("FUEL", 1)];
+    let init_req = reactions.requirements(&target_term);
+    let mut input: Vec<Term> = init_req.0;
+    let mut output: Vec<Term> = vec![init_req.1];
 
     loop {
         input = input
@@ -110,7 +101,7 @@ fn main() {
                     return vec![x.clone()];
                 } else {
                     let (i, o) = reactions.requirements(x);
-                    println!("add out {}: ({:?}) -> ({})", x, i, o);
+                    // println!("add out {}: ({:?}) -> ({})", x, i, o);
                     if o.num > x.num {
                         output.push(Term::new(&x.element, o.num - x.num));
                     }
@@ -136,7 +127,56 @@ fn main() {
             break;
         }
     }
+    assert_eq!(input.len(), 1);
+    assert_eq!(input[0].element, "ORE");
+    // println!("input: {:?}", input);
+    // println!("output: {:?}", output);
 
-    println!("input: {:?}", input);
-    println!("output: {:?}", output);
+    input[0].num
+}
+fn main() {
+    let mut reactions = HashMap::new();
+    for line in stdin().lock().lines() {
+        let line = line.unwrap();
+        let mut split = line.split("=>"); //.collect();
+        let input = split.next().unwrap();
+        let output = Term::from_str(split.next().unwrap());
+        let input_elements: Vec<_> = input.split(",").map(|x| Term::from_str(&x[..])).collect();
+
+        reactions.insert(output, input_elements);
+    }
+    println!("{:?}", reactions);
+    let mut target_term = Term::new("FUEL", 1);
+    let mut lower = 0;
+    let mut upper = 1;
+
+    loop {
+        let num_ore = eval(&target_term, &reactions);
+        if num_ore < 1000000000000 {
+            // new_lower =
+            lower = upper;
+            upper *= 2;
+            target_term = Term::new("FUEL", upper);
+            println!("below");
+        } else if num_ore > 1000000000000 {
+            break;
+        }
+    }
+    loop {
+        let mid = (upper + lower) / 2;
+        target_term = Term::new("FUEL", mid);
+
+        let num_ore = eval(&target_term, &reactions);
+
+        if num_ore < 1000000000000 {
+            lower = mid;
+        } else if num_ore > 1000000000000 {
+            upper = mid;
+        }
+        println!("{} {} {} {}", lower, upper, num_ore, target_term);
+
+        if (upper - lower <= 1) {
+            break;
+        }
+    }
 }
